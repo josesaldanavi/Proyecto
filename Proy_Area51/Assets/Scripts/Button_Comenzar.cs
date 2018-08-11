78b8bdb5c03e29c3e038fd14f3966df1efd184b1
Jose Saldaña <josesaldanavi@gmail.com>
2018-08-11 00:23:40 -0500

    Falta mejorar fundido

---
+++ Proy_Area51/Assets/Scripts/Button_Comenzar.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Button_Comenzar : MonoBehaviour {
+
+    public Image fundido;
+    public string[] escenas;
+
+	void Start () {
+        fundido.CrossFadeAlpha(0, 0.5f, false);
+	}
+	
+    public void FadeOut(int s)
+    {
+        fundido.CrossFadeAlpha(1, 0.5f, false);
+        StartCoroutine(CambioEscena(escenas[s]));
+    }
+
+    IEnumerator CambioEscena(string escena)
+    {
+        yield return new WaitForSeconds(1);
+        SceneManager.LoadScene(escena);
+    }
+
+}
