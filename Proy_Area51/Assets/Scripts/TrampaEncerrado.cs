b5a75c9f002166ddf0bedc8306293516ac956a92
daniel alexander nacarino <kkmon244959@gmail.com>
2018-08-14 21:35:32 -0500

    Con Trampa en inicio

---
+++ Proy_Area51/Assets/Scripts/TrampaEncerrado.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrampaEncerrado : MonoBehaviour {
+    public int health=20;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        /*if(Input.GetKeyDown(KeyCode.Tab)){
+            TakeDamage();
+        }*/
+	}
+
+    private void TakeDamage(){
+        health--;
+        if(health<=0){
+            Break();
+        }
+    }
+
+    private void Break(){
+        //Momentaneo. Despues DestroyThis se va a llamar a fin de la animacion como evento
+        //Y aqui solo se llama a la animacion
+        DestroyThis();
+    }
+
+    private void DestroyThis(){
+        Destroy(gameObject);
+    }
+
+    public void CallOnCollisionEnter2D()
+	{
+        
+            TakeDamage();
+
+	}
+}
---
+++ Proy_Area51/Assets/Scripts/TrampaHijo.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrampaHijo : MonoBehaviour {
+
+    public TrampaEncerrado trampa;
+
+	public void OnCollisionEnter2D(Collision2D collision)
+	{
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            trampa.CallOnCollisionEnter2D();
+        }
+	}
+
+}
