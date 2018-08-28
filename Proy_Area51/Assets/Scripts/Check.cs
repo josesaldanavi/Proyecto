94c2f88841e84cc2e22c966c6a3b21ecdb1daed9
Jose Saldaña <josesaldanavi@gmail.com>
2018-08-28 16:58:40 -0500

    Arreglar Platform falling

---
+++ Proy_Area51/Assets/Scripts/Check.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Check : MonoBehaviour {
+
+    private Movement player;
+    private Rigidbody2D rb2d;
+
+    void Start()
+    {
+        player = GetComponentInParent<Movement>();
+        rb2d = GetComponentInParent<Rigidbody2D>();
+    }
+
+    private void OnCollisionStay2D(Collision2D col)
+    {
+
+        if (col.gameObject.tag == "platform")
+        {
+            player.transform.parent = col.transform;
+
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "platform")
+        {
+            player.transform.parent = null;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+
+        if (collision.gameObject.tag == "platform")
+        {
+            player.transform.parent = null;
+        }
+    }
+}
---
+++ Proy_Area51/Assets/Scripts/Falling_platform.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Falling_platform : MonoBehaviour {
+
+    private Rigidbody2D rbd2d;
+    private PolygonCollider2D pol;
+    public float time = 1f;
+    public float respawnDelay = 1f;
+    Vector3 start;
+
+    // Use this for initialization
+    void Start()
+    {
+        rbd2d = GetComponent<Rigidbody2D>();
+        pol = GetComponent<PolygonCollider2D>();
+        start = transform.position;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            //Invoca un mentodo y lo hace despues de un cierto tiempo
+            Invoke("Fall", time);
+            Invoke("Respawn", time + respawnDelay);
+        }
+    }
+
+    void Fall()
+    {
+        //hace que sea dinamico automaticamente
+        rbd2d.isKinematic = false;
+        pol.isTrigger = true;
+    }
+    void Respawn()
+    {
+        transform.position = start;
+        rbd2d.isKinematic = true;
+        pol.isTrigger = false;
+        rbd2d.velocity = Vector3.zero;
+    }
+}
