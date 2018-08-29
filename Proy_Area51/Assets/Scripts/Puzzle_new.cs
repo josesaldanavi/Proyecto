e427160fe20376b1e045ba8a49f786475ff1a339
josesaldanavi <josesaldanavi@gmail.com>
2018-08-28 22:00:22 -0500

    arreglar puzzle(jose)

---
+++ Proy_Area51/Assets/Scripts/Puzzle_new.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Puzzle_new : MonoBehaviour {
+    public GameObject[] other;
+    //public bool check=false;
+    int i = 0;
+    // Use this for initialization
+    void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.A) && i<=5)
+        {
+            SwapRed();
+            i++;
+        }
+        if (i == 4)
+        {
+            i = 0;
+        }
+	}
+ 
+
+    void SwapRed()
+    {
+            Vector3 temp = transform.position;
+            transform.position = other[i].transform.position;
+             other[i].transform.position = temp;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+
+            
+        }
+    }
+}
