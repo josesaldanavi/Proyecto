39378c8163aa6bd81ec0d238f0773f6d17ff0580
Jose Saldaña <josesaldanavi@gmail.com>
2018-08-30 10:54:50 -0500

    script puzzle new_arreglar

--- Proy_Area51/Assets/Scripts/Puzzle_new.cs
+++ Proy_Area51/Assets/Scripts/Puzzle_new.cs
@@ -3,41 +3,69 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Puzzle_new : MonoBehaviour {
-    public GameObject[] other;
-    //public bool check=false;
+    public GameObject[] puzzle;
+    public float speed=0.1f;
     int i = 0;
+    public bool aumenta = false;
+    public GameObject PuzzleController;
     // Use this for initialization
     void Start () {
-
+        puzzle[i].GetComponent<Transform>();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-        if (Input.GetKeyDown(KeyCode.A) && i<=5)
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.W))
         {
-            SwapRed();
+            puzzle[i].transform.Translate(Vector2.up * speed * Time.deltaTime);
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            puzzle[i].transform.Translate(Vector2.down * speed * Time.deltaTime);
+
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            puzzle[i].transform.Translate(Vector2.left * speed * Time.deltaTime);
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            puzzle[i].transform.Translate(Vector2.right * speed * Time.deltaTime);
+
+        }
+        AumentaContador();
+    }
+
+
+    void AumentaContador()
+    {
+        if (aumenta == true)
+        {
             i++;
-        }
-        if (i == 4)
-        {
-            i = 0;
-        }
-	}
- 
+            aumenta = false;
 
-    void SwapRed()
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        other = PuzzleController.GetComponent<Collider2D>();
+        if (aumenta == false && other.tag=="Meta")
+        {
+            puzzle[i].gameObject.transform.parent = PuzzleController.transform;
+            aumenta = true;
+        }
+    }
+
+
+    /*void SwapRed()
     {
             Vector3 temp = transform.position;
             transform.position = other[i].transform.position;
              other[i].transform.position = temp;
-    }
+    }*/
 
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.CompareTag("Player"))
-        {
 
-            
-        }
-    }
 }
