32d4bc24629ed4f450e0a1078dfd5222dc32e8f6
Jose Saldaña <josesaldanavi@gmail.com>
2018-09-03 00:08:24 -0500

    splash screen

---
+++ Proy_Area51/Assets/Scripts/Splash_screen.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Splash_screen : MonoBehaviour {
+
+    public GameObject logo;
+    public  int scene_cont;
+
+	void Start () {
+        StartCoroutine(SplashScreen());
+	}
+	
+    IEnumerator SplashScreen()
+    {
+        yield return new WaitForSeconds(0.5f);
+        logo.SetActive(true);
+        yield return new WaitForSeconds(4.5f);
+        SceneManager.LoadScene(scene_cont);
+    }
+
+}
