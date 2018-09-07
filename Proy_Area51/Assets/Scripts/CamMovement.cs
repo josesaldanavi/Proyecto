cb5a234a7e1701d119478805cba9acd3f9442e20
setseroZZ <resetos35@gmail.com>
2018-09-06 19:35:14 -0500

    Con script de camara

---
+++ Proy_Area51/Assets/Scripts/CamMovement.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamMovement : MonoBehaviour
+{
+
+    public Transform targetObject;
+    public Color targetColor;
+    public float distance = 1;
+    public float maxDistanceDelta = 1;
+
+    public float speed = 0;
+    float deaccel = 45;
+    public Vector3 impulseDirection;
+
+    // Use this for initialization
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (speed != 0)
+        {
+            speed = Mathf.MoveTowards(speed, 0, deaccel * Time.deltaTime);
+        }
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector3 targetCamPos = targetObject.position;
+        Vector3 currentCamPos = transform.position;
+        targetCamPos.z = transform.position.z;
+        float currentDistance = Vector3.Distance(currentCamPos, targetCamPos);
+
+        transform.Translate(impulseDirection * speed * Time.deltaTime);
+
+        transform.position = Vector3.MoveTowards(transform.position, targetCamPos, maxDistanceDelta * currentDistance * Time.deltaTime);
+    }
+
+    /*void OnDrawGizmos()
+    {
+        Gizmos.color = targetColor;
+        Vector3 targetViewPos = (targetScript != null) ? targetObject.position + (targetScript.sightDirection.up * distance) : Vector3.zero;
+        Gizmos.DrawWireSphere(targetViewPos, 0.5f);
+        Gizmos.color = Color.red;
+        Vector3 currentViewPos = new Vector3(transform.position.x, transform.position.y, 0);
+        Gizmos.DrawWireSphere(currentViewPos, 0.5f);
+        Gizmos.color = Color.green;
+        Gizmos.DrawLine(currentViewPos, targetViewPos);
+    }*/
+}
--- Proy_Area51/Assets/Scripts/TrampaConClick.cs
+++ Proy_Area51/Assets/Scripts/TrampaConClick.cs
@@ -2,46 +2,55 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TrampaConClick : MonoBehaviour {
+public class TrampaConClick : MonoBehaviour
+{
     public int health = 10;
     public Movement player;
     public GameObject sealSprite;
-
     public GameObject notPlayer;
 
     private bool isActiveAndReady;
-	// Use this for initialization
+
+    public CamMovement camera;
+    // Use this for initialization
     void Awake()
     {
         /*Debug.Log("Object con tag",GameObject.FindGameObjectWithTag("Player").name);
         player = (GameObject.FindGameObjectWithTag("Player")).GetComponent<Movement>();*/
-        notPlayer=GameObject.FindGameObjectWithTag("Player");
+        notPlayer = GameObject.FindGameObjectWithTag("Player");
 
     }
 
-	void Start () {
+    void Start()
+    {
         //Por ahora va a activarse cuando comienza.
         //Cambiar logica si se desea utilizarlo diferente despues.
         Activate();
 
     }
-	
-	// Update is called once per frame
-	void Update () {
-        
+
+    // Update is called once per frame
+    void Update()
+    {
+
     }
 
     void OnMouseOver()
     {
-        if (Input.GetMouseButtonDown(0)&&isActiveAndReady) {
+        if (Input.GetMouseButtonDown(0) && isActiveAndReady)
+        {
             TakeDamage();
-            if (health == 0){
+            camera.speed = 5;
+            camera.impulseDirection = GetRandomDirection(360);
+            camera.impulseDirection = Vector3.up;
+            if (health == 0)
+            {
                 print("Destroyed!");
                 player.releaseFromCurse();
                 DestroyThis();
             }
 
-            print("Falta hacer click "+health+" vece(s)");
+            print("Falta hacer click " + health + " vece(s)");
         }
         //Debug.Log("Mouse is over GameObject.");
     }
@@ -50,15 +59,24 @@ public class TrampaConClick : MonoBehaviour {
     {
         health--;
     }
-    private void DestroyThis() {
+    private void DestroyThis()
+    {
         Destroy(gameObject);
         Destroy(sealSprite);
     }
-    
+
     public void Activate()
     {
         player.sealThis();
         isActiveAndReady = true;
 
     }
+
+    Vector3 GetRandomDirection(float maxAngle)
+    {
+        float randomAngle = Random.Range(0f, maxAngle);
+        Quaternion quat = Quaternion.Euler(new Vector3(0, 0, randomAngle));
+        return quat * Vector3.forward;
+    }
 }
+
