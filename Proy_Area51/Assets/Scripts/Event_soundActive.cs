cd64baeb4105404d48b8fedfc69656c2634640a1
Jose Saldaña <josesaldanavi@gmail.com>
2018-09-16 00:34:47 -0500

    sonido de game over

---
+++ Proy_Area51/Assets/Scripts/Event_soundActive.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Event_soundActive : MonoBehaviour {
+
+    public void Event_ActivarSound()
+    {
+        GetComponent<AudioSource>().Play();
+    }
+
+}
