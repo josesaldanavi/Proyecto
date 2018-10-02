36580561c2acbe94f0e21056f6e99a18b45f7aa7
Jose Saldaña <josesaldanavi@gmail.com>
2018-10-02 00:53:06 -0500

    Arrreglar enemigo

--- Proy_Area51/Assets/Pausa_script.cs
+++ Proy_Area51/Assets/Pausa_script.cs
@@ -2,15 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class Pausa_script : MonoBehaviour {
     bool activado;
     public AudioMixer audioMixer;
+    public Dropdown resolutionDropdowm;
+    Resolution[] resolutions;
    public static Canvas canvas;
 	// Use this for initialization
 	void Start () {
         canvas = GetComponent<Canvas>();
         canvas.enabled = false;
+        resolutions = Screen.resolutions;
+
+        resolutionDropdowm.ClearOptions();
+        List<string> options = new List<string>();
+        int currentResolutionIndex = 0;
+        for(int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + "x" + resolutions[i].height;
+            options.Add(option);
+            if(resolutions[i].width==Screen.currentResolution.width &&
+                resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+
+        resolutionDropdowm.AddOptions(options);
+        resolutionDropdowm.value = currentResolutionIndex;
+        resolutionDropdowm.RefreshShownValue();
 	}
     private void Awake()
     {
@@ -25,9 +47,23 @@ public class Pausa_script : MonoBehaviour {
             Time.timeScale = (activado) ? 0 : 1f;
         }
 	}
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);   
+    }
 
     public void setVolume(float volume)
     {
         audioMixer.SetFloat("Volume", volume);
+    }   
+
+    public void SetQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+    public void SetFullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
     }
 }
